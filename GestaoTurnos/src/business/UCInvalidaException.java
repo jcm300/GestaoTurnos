89a1082e776b1f6a11605602530ed346a0cd8b71
jcm300 <zeclmartins@gmail.com>
2017-12-30 15:04:58 +0000

    removed unecessary comments

--- GestaoTurnos/src/business/UCInvalidaException.java
+++ GestaoTurnos/src/business/UCInvalidaException.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package business;
 
-/**
- *
- * @author jonasv
- */
 public class UCInvalidaException extends Exception {
     
     public UCInvalidaException(){
